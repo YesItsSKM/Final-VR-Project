982d5d16f634745059b2348a67fda96bd353afd9
Shubham Kumar Maurya <yesitsskm@gmail.com>
2022-07-08 10:55:36 +0100

    Added basic controller hands.

---
+++ Assets/Scripts/HarnessGravity.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HarnessGravity : MonoBehaviour
+{
+    public XRRayInteractor leftHandRay, rightHandRay;
+
+    [SerializeField] Transform obj;
+    Rigidbody objRigidBody;
+
+    public float influenceRange;
+    public float intensity;
+    public float distanceToPlayer;
+    
+    Vector3 pullForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftHandRay.TryGetCurrent3DRaycastHit(out RaycastHit hit) && (hit.transform.tag == "Interactable"))
+        {
+            obj = hit.transform;
+            objRigidBody = hit.rigidbody;
+
+            distanceToPlayer = Vector3.Distance(obj.position, transform.position);
+
+            if (distanceToPlayer <= influenceRange)
+            {
+                pullForce = (transform.position - obj.position).normalized / distanceToPlayer * intensity;
+                objRigidBody.AddForce(pullForce, ForceMode.Force);
+            }
+        }
+    }
+}
